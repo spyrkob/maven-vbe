744237d53f2a035d64964c4ed2405fb72adba2ce
Chao Wang <chaowan@redhat.com>
2022-03-25 15:07:54 +0800

    Fix #1 RedhatVersionAcceptor should accept -redhat- version

--- src/main/java/org/jboss/set/maven/release/extension/version/RedhatVersionAcceptor.java
+++ src/main/java/org/jboss/set/maven/release/extension/version/RedhatVersionAcceptor.java
@@ -1,10 +1,15 @@
 package org.jboss.set.maven.release.extension.version;
 
+import java.util.regex.Pattern;
+
 public class RedhatVersionAcceptor implements VersionAcceptanceCriteria {
 
+    // redhat prod version "-redhat-" and ".redhat-"
+    public static Pattern redhatPattern = Pattern.compile("[\\.|-]redhat-");
+
     @Override
-    public boolean accept(final String current,final String proposed) {
-        return proposed!=null && proposed.contains(".redhat-");
+    public boolean accept(final String current, final String proposed) {
+        return proposed != null && redhatPattern.matcher(proposed).find();
     }
 
 }
